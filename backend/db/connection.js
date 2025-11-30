61a1b47c524b5ea69b29601107b1f60eeec2aa08
Lisandro Etcheverry <lisandro@example.com>
2025-11-30 18:20:09 -0300

    Fix: Usuario correcto para Supabase Pooler

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -9,7 +9,7 @@ const pool = new Pool({
     host: 'aws-0-sa-east-1.pooler.supabase.com',
     port: 6543,
     database: 'postgres',
-    user: 'postgres.bqlppayfgsepdrepenxt',
+    user: 'postgres',
     password: 'Cocoliso13!',
     ssl: {
         rejectUnauthorized: false
